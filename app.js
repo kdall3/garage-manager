febb9b87253c0847c4a4f9cdf1aa85284c4edbe4
Kai Dallison <kai.dallison@student.manchester.ac.uk>
2025-12-11 11:01:04 +0000

    Added check for existance of .env file

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
+const fs = require('fs');
+const path = require(`path`);
 const express = require('express');
 const session = require('express-session');
-const path = require('path');
-require('dotenv').config();
+
+// Check if .env exists
+const envPath = path.resolve(__dirname, '.env');
+
+if (fs.existsSync(envPath)) {
+    require('dotenv').config();
+} else {
+    // Formatting string makes it red
+    console.warn('\x1b[31m%s\x1b[0m', 'ERROR: .env file not found');
+    throw new Error('.env file not found');
+}
 
 const requireLogin = require('./routes/login')
 
 const app = express();
 app.set('view engine', 'ejs');
-
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
