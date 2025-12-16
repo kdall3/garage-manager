4e107eb654caad72d77cee622dcb7008a0d2f47f
Spottington <ellis.potts@lishman.co.uk>
2025-12-16 20:54:13 +0000

    Transactions And add page frame

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -9,19 +9,9 @@ transactionsRouter
         res.render('transactions/transactions', {
         });
     })
-        
 
-// transactionsRouter
-//     .route('/add')
-//     .get(requireLogin, (req: Request, res: Response) => {
-//         res.render('transactions/add', {
-//             form_values: req.session.form_values ?? {},
-//             input_errors: req.session.input_errors ?? {},
-//             success_message: req.session.success_message ?? ''
-//         });
-
-//         // Reset form session vars after we are done rendering them
-//         req.session.input_errors = {};
-//         req.session.success_message = '';
-//     })
-//     .post(requireLogin, async (req: Request, res: Response) => {})
+transactionsRouter
+  .route('/add')  
+  .get(requireLogin, (req: Request, res: Response) => {
+    res.render("transactions/add");
+  });
