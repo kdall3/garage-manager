7e19264074e15d87857fd028642a26add01082ba
Spottington <ellis.potts@lishman.co.uk>
2026-01-04 03:25:24 +0000

    To Do List on dashboard framework

---
+++ src/db/tasks.ts
@@ -0,0 +1,59 @@
+import { db } from ".";
+import { getCarFromReg } from "./cars";
+import { getEmployeeById, Employee } from './user';
+
+// Employee name to list of shifts
+export interface Task {
+    task_id: number,
+    task: string,
+    reg_plate: string,
+    priority: number  
+}
+
+export async function getActiveTasks(reg_plate?: string): Promise<Task[]> {
+    if(!reg_plate) {
+        const [rows] = await db.query<Task>(
+            `
+            SELECT task_id, task, reg_plate, priority, active 
+            FROM tasks
+            ORDER BY active DESC, priority
+            `
+        );
+        return rows;
+    } else {
+        const [rows] = await db.query<Task>(
+            `
+            SELECT task_id, task, reg_plate, priority, active
+            FROM tasks
+            WHERE reg_plate=?
+            ORDER BY active DESC, priority
+            `,
+            [reg_plate]
+        );
+        return rows;
+    }
+}
+
+export async function toggleActive(task_id: number, active: boolean): Promise<void> {
+    
+    if (active) {
+        await db.query(
+            `
+            UPDATE tasks
+            SET active=FALSE
+            WHERE task_id=?
+            `,
+            [task_id]
+    );
+    } else {
+        await db.query(
+            `
+            UPDATE tasks
+            SET active=TRUE
+            WHERE task_id=?
+            `,
+            [task_id]
+    );
+    }
+    
+}
--- src/routes/dashboard.ts
+++ src/routes/dashboard.ts
@@ -2,6 +2,8 @@ import express, { Request, Response, NextFunction } from "express";
 import { requireLogin } from "../middleware/requireLogin";
 import { getCars } from "../db/cars";
 import { hoursPerCar } from "../db/hours";
+import { getActiveTasks } from "../db/tasks";
+import { toggleActive } from "../db/tasks";
 
 export const dashboardRouter = express.Router();
 
@@ -10,11 +12,22 @@ dashboardRouter
     .get(requireLogin, async (req: Request, res: Response) => {
         const cars = await getCars();
         const carsInStock = cars.filter(car => car.status !== 'Sold');
-
+        const activeTasks = await getActiveTasks();
         const carHours = await hoursPerCar();
 
         res.render("dashboard", {
             carsInStock,
-            carHours
+            carHours,
+            activeTasks
         });
-    });
+    })
+    .post(requireLogin, async (req: Request, res: Response) => {
+        const { task_id, active } = req.body;
+
+        await toggleActive(
+            Number(task_id),
+            Boolean(active)
+        );
+
+        res.redirect(req.get("Referer") || "/");
+        });
